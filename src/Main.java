0722f747779cec053632f2654a2be384601df114
Harry Yoon <45408584+H-O-Y@users.noreply.github.com>
2024-05-20 11:31:36 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // Scanner scanner = new Scanner(System.in);
 
         //Creates 3x3 Grid
-        public Board[][] data = new Board[3][3];
+        Board[][] data = new Board[3][3];
         for (int r = 0; r < 3; r++) {
             for (int c = 0; c < 3; c++) {
                 data[r][c] = new Board(3, 3);
@@ -27,27 +27,27 @@ public class Main {
         int currentPlayer = 1; // 1 for X, -1 for O
         boolean gameOver = false;
 
+        int bRow = 0, bCol = 0, sRow = 0, sCol = 0;
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter row col");
-        int bRow = scan.nextInt();
-        int bCol = scan.nextInt();
-        System.out.println("enter row col");
-        int sRow = scan.nextInt();
-        int sCol = scan.nextInt();
 
         data[bRow][bCol].modifyBoard(sRow, sCol, currentPlayer);
 
-        while (gameOver == false) {
-            //add latency
-            
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            frame.repaint();
-        }
+        System.out.println("enter row col");
+        bRow = scan.nextInt();
+        bCol = scan.nextInt();
+        System.out.println("enter row col");
+        sRow = scan.nextInt();
+        sCol = scan.nextInt();
+        data[bRow][bCol].modifyBoard(sRow, sCol, currentPlayer);
+        frame.repaint();
+        System.out.println("enter row col");
+        bRow = scan.nextInt();
+        bCol = scan.nextInt();
+        System.out.println("enter row col");
+        sRow = scan.nextInt();
+        sCol = scan.nextInt();
+        data[bRow][bCol].modifyBoard(sRow, sCol, currentPlayer);
+        frame.repaint();
 
         
     }
