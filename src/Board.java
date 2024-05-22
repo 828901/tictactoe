35fba52707c047a1c42e39583d819d68b92fac47
Harry Yoon <45408584+H-O-Y@users.noreply.github.com>
2024-05-22 13:11:20 -0400

    Update Board.java
    added comments

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,12 @@
+// Purpose: To store the game state of each individual tic-tac-toe board as a 2D array of integers.
+// Authors: Harry and Noah
 public class Board {
-
     private int winStatus = 0;
-
     private int[][] board = new int[3][3];
 
+    // Author: Noah
+    // Precondition: N/A
+    // Postcondition: A new board objects is created and int[][] board is filled with 0s.
     public Board() {
         for(int i = 0; i < 3; i++){
             for(int x = 0; x < 3; x++){
@@ -12,19 +15,37 @@ public class Board {
         }
     }
 
+    // Author: Noah
+    // Precondition: board exists.
+    // Postcondition: board is returned.
+    // @return the 2D array of integers known as board.
     public int[][] getBoard(){
         return board;
     }
+
+    // Author: Noah
+    // Precondition: board exists and x and y are integers between 0 an 2 (inclusive)
+    // Postcondition: The integer at a specific index in board is returned.
+    // @param x  an integer representing the row of the board being accessed
+    // @param y  an integer representing the column of the board being accessed
+    // @return the integer at a specific index in board is returned.
     public int getValue(int x, int y){
         return board[x][y];
     }
-
-
-
+    
+    // Author: Noah
+    // Precondition: board exists.
+    // Postcondition: The integer at board[x][y] is replaced with value.
+    // @param x  an integer representing the row of the board being changed.
+    // @param y  an integer representing the column of the board being changed.
     public void modifyBoard(int x, int y, int value){
         board[x][y] = value;
     }
 
+    // Author: Noah
+    // Precondition: board exists.
+    // Postcondition: A String representing the game state of a board is returned.
+    // @return a String representing the game state of a board is returned.
     public String toString(){
         String string = "";
         for(int x = 0; x < 3; x++){
@@ -33,13 +54,20 @@ public class Board {
             }
         }
         return string;
-
     }
-
+    
+    // Author: Noah
+    // Precondition: board exists.
+    // Postcondition: winStatus is returned
+    // @return winStatus  0 if no player has won; 1 if x has won; -1 if 0 has won.
     public int getWinStatus(){
         return winStatus;
     }
-
+    
+    // Author Harry
+    // Precondition: board exists
+    // Postcondition: The following is returned: 0 if no player has won; 1 if x has won; -1 if 0 has won.
+    // @return 0 if no player has won; 1 if x has won; -1 if 0 has won.
     public int checkForWin() {
         // Check rows
         for (int i = 0; i < 3; i++) {
