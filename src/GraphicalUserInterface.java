3d1484d18ba185037dd793983919716c986cf8c3
ParkerL72 <166850553+ParkerL72@users.noreply.github.com>
2024-05-22 14:02:18 -0400

    Update GraphicalUserInterface.java, not finished
    comments

--- src/GraphicalUserInterface.java
+++ src/GraphicalUserInterface.java
@@ -4,6 +4,8 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Arrays;
 
+//Class Author: Noah, Harry
+//Class Purpose: Display a graphical user interface of the board to the user and registers user mouse inputs for each turn of the game. 
 public class GraphicalUserInterface extends JPanel {
 
     private static final int NUM_BOARDS = 3;
@@ -18,13 +20,22 @@ public class GraphicalUserInterface extends JPanel {
 
     private int[][] boardsWon;
 
-    public GraphicalUserInterface(Board[][] boards, int[][] boardsWonIn) {
+    //Author: Harry, Noah
+    //Precondition: X and Y exist when mouse
+    //Postcondition: Creates a new GUI as well as detector for when the mouse is clicked. 
+    //@para: The board, 
+    public GraphicalUserInterface(Board[][] boards, int[][] boardsWonIn) { //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX boardsWonIn as param?
         boardsWon = boardsWonIn;
         this.boards = boards;
         setPreferredSize(new Dimension(NUM_BOARDS * BOARD_SIZE * CELL_SIZE, NUM_BOARDS * BOARD_SIZE * CELL_SIZE));
         setBackground(Color.WHITE);
         addMouseListener(new MouseAdapter() {
-          @Override
+
+            //Author: Harry, Noah
+            //Precondition: X and Y exist where mouse is clicked. 
+            //Postcondition: The GUI is updated with the users input at the location of the mouse clicked. 
+            //@para: Mouse clicked
+            @Override
           public void mouseClicked(MouseEvent e) {
             int x = e.getX();
             int y = e.getY();
@@ -36,6 +47,10 @@ public class GraphicalUserInterface extends JPanel {
         
     }
 
+    //Author: Harry
+    //Precondition: X and Y are not beyond the borders of the board. 
+    //Postcondition: the board/cell row and collumn are updated to the users location. XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX double check w/ harry
+    //@para: The locations of x and y where the mouse is clicked. 
     private void registerUserInput(int x, int y) {
         // Determine the larger board
         boardRow = y / (BOARD_SIZE * CELL_SIZE);
@@ -44,14 +59,12 @@ public class GraphicalUserInterface extends JPanel {
         // Determine the cell within the smaller board
         cellRow = (y % (BOARD_SIZE * CELL_SIZE)) / CELL_SIZE;
         cellCol = (x % (BOARD_SIZE * CELL_SIZE)) / CELL_SIZE;
-
-        // UPDATE THE BOARD HERE
-
-        //System.out.println("\nLarger board row: " + boardRow + ", col: " + boardCol);
-        //System.out.println("Smaller board row: " + cellRow + ", col: " + cellCol);
-        
       }
     
+    //Author: Harry
+    //Precondition: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX WTF IS THE HIGHLIGHT ROW/COL for PRE 
+    //Postcondition: The GUI's visual aspect is created. If a user has won a board, a large symbol (X/O) is drawn in accordance to the user that won. 
+    //@para: The GUI 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -118,6 +131,10 @@ public class GraphicalUserInterface extends JPanel {
     }
 
     // Method to draw X or O in a cell
+    //Author: Harry
+    //Precondition: The board/cell row and collumn exist and are not larger or smaller than the board size. 
+    //Postcondition: An "X" is drawn in the specific cell. 
+    //@para: The GUI of the board, the board/cell row and collumn.  
     private void drawX(Graphics2D g2d, int boardRow, int boardCol, int cellRow, int cellCol) {
         g2d.setStroke(new BasicStroke(4f));
         int x = boardCol * BOARD_SIZE * CELL_SIZE + cellCol * CELL_SIZE;
@@ -125,12 +142,22 @@ public class GraphicalUserInterface extends JPanel {
         g2d.drawLine(x + 10, y + 10, x + CELL_SIZE - 10, y + CELL_SIZE - 10);
         g2d.drawLine(x + CELL_SIZE - 10, y + 10, x + 10, y + CELL_SIZE - 10);
     }
+    
+    //Author: Harry
+    //Precondition: The board/cell row and collumn exist and are not larger or smaller than the board size. 
+    //Postcondition: An "O" is drawn in the specific cell. 
+    //@para: The GUI of the board, the board/cell row and collumn.  
     private void drawO(Graphics2D g2d, int boardRow, int boardCol, int cellRow, int cellCol) {
         g2d.setStroke(new BasicStroke(4f));
         int x = boardCol * BOARD_SIZE * CELL_SIZE + cellCol * CELL_SIZE;
         int y = boardRow * BOARD_SIZE * CELL_SIZE + cellRow * CELL_SIZE;
         g2d.drawOval(x + 10, y + 10, CELL_SIZE - 20, CELL_SIZE - 20);
     }
+    
+    //Author: Harry
+    //Precondition: The board row and collumn exist and are not larger or smaller than the board size. 
+    //Postcondition: An "X" is drawn in the specific cell. 
+    //@para: The GUI of the board, the board row and collumn.  
     private void drawBigRedX(Graphics2D g2d, int boardRow, int boardCol) {
         boardsWon[boardRow][boardCol] = 1;
         g2d.setColor(Color.RED);
@@ -141,6 +168,11 @@ public class GraphicalUserInterface extends JPanel {
         g2d.drawLine(x + BOARD_SIZE * CELL_SIZE - 10, y + 10, x + 10, y + BOARD_SIZE * CELL_SIZE - 10);
         g2d.setColor(Color.BLACK);
       }
+    
+    //Author: Harry
+    //Precondition: The board row and collumn exist and are not larger or smaller than the board size. 
+    //Postcondition: An "O" is drawn in the specific cell. 
+    //@para: The GUI of the board, the board row and collumn. 
     private void drawBigRedO(Graphics2D g2d, int boardRow, int boardCol) {
         boardsWon[boardRow][boardCol] = -1;
         g2d.setColor(Color.RED);
@@ -151,10 +183,19 @@ public class GraphicalUserInterface extends JPanel {
         g2d.setColor(Color.BLACK);
     }
 
+    //Author: Noah
+    //Precondition: The board row and collumn exist and are not larger or smaller than the board size. 
+    //Postcondition: The cell for which the user needs to make the next move on the board is highlighted. 
+    //@para: The GUI of the board, the board row and collumn. 
     public void drawHighlight(Graphics g, int row, int col){
         hightlightRow = row;
         hightlightCol = col;
     }
+    
+    //Author: Noah
+    //Precondition: The board row and collumn exist and are not larger or smaller than the board size. 
+    //Postcondition: The board is updated with the inputed board. 
+    //@para: The board. 
     private void updateBoard(Board[][] boards) {this.boards = boards;}
     
 }
