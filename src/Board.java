bb7f2f8870ffa94b2f3540342df83f6a4f6b0ca0
Harry Yoon <45408584+H-O-Y@users.noreply.github.com>
2024-05-24 12:49:01 -0400

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -6,10 +6,11 @@ public class Board {
 
     // Author: Noah
     // Precondition: N/A
-    // Postcondition: A new board objects is created and int[][] board is filled with 0s.
+    // Postcondition: A new board objects is created and int[][] board is filled
+    // with 0s.
     public Board() {
-        for(int i = 0; i < 3; i++){
-            for(int x = 0; x < 3; x++){
+        for (int i = 0; i < 3; i++) {
+            for (int x = 0; x < 3; x++) {
                 board[i][x] = 0;
             }
         }
@@ -19,26 +20,27 @@ public class Board {
     // Precondition: board exists.
     // Postcondition: board is returned.
     // @return the 2D array of integers known as board.
-    public int[][] getBoard(){
+    public int[][] getBoard() {
         return board;
     }
 
     // Author: Noah
-    // Precondition: board exists and x and y are integers between 0 an 2 (inclusive)
+    // Precondition: board exists and x and y are integers between 0 an 2
+    // (inclusive)
     // Postcondition: The integer at a specific index in board is returned.
-    // @param x  an integer representing the row of the board being accessed
-    // @param y  an integer representing the column of the board being accessed
+    // @param x an integer representing the row of the board being accessed
+    // @param y an integer representing the column of the board being accessed
     // @return the integer at a specific index in board is returned.
-    public int getValue(int x, int y){
+    public int getValue(int x, int y) {
         return board[x][y];
     }
-    
+
     // Author: Noah
     // Precondition: board exists.
     // Postcondition: The integer at board[x][y] is replaced with value.
-    // @param x  an integer representing the row of the board being changed.
-    // @param y  an integer representing the column of the board being changed.
-    public void modifyBoard(int x, int y, int value){
+    // @param x an integer representing the row of the board being changed.
+    // @param y an integer representing the column of the board being changed.
+    public void modifyBoard(int x, int y, int value) {
         board[x][y] = value;
     }
 
@@ -46,27 +48,28 @@ public class Board {
     // Precondition: board exists.
     // Postcondition: A String representing the game state of a board is returned.
     // @return a String representing the game state of a board is returned.
-    public String toString(){
+    public String toString() {
         String string = "";
-        for(int x = 0; x < 3; x++){
-            for(int y = 0; y < 3; y++){
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
                 string += board[x][y] + ", ";
             }
         }
         return string;
     }
-    
+
     // Author: Noah
     // Precondition: board exists.
     // Postcondition: winStatus is returned
-    // @return winStatus  0 if no player has won; 1 if x has won; -1 if 0 has won.
-    public int getWinStatus(){
+    // @return winStatus 0 if no player has won; 1 if x has won; -1 if 0 has won.
+    public int getWinStatus() {
         return winStatus;
     }
-    
+
     // Author Harry
     // Precondition: board exists
-    // Postcondition: The following is returned: 0 if no player has won; 1 if x has won; -1 if 0 has won.
+    // Postcondition: The following is returned: 0 if no player has won; 1 if x has
+    // won; -1 if 0 has won.
     // @return 0 if no player has won; 1 if x has won; -1 if 0 has won.
     public int checkForWin() {
         // Check rows
@@ -75,8 +78,7 @@ public class Board {
             if (rowSum == 3) {
                 winStatus = 1;
                 return 1; // Player with 'X' has won
-            }
-            else if (rowSum == -3) {
+            } else if (rowSum == -3) {
                 winStatus = -1;
                 return -1; // Player with 'O' has won
             }
@@ -101,5 +103,20 @@ public class Board {
 
         // No winner yet
         return 0;
+    }
+
+    // Author Harry
+    // Precondition: board exists
+    // Postcondition: Return true if the board is full, false otherwise.
+    // @return true if the board is full, false otherwise.
+    public boolean checkFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
